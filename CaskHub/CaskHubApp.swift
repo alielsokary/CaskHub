add0dd10ede8c08fd6d50e9425c1c617853f129f
Ali Elsokary <ali.elsokary.w@gmail.com>
2026-02-08 01:13:47 +0200

    Init Project

---
+++ CaskHub/CaskHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaskHubApp.swift
+//  CaskHub
+//
+//  Created by Ali Elsokary on 08/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CaskHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
